b383aa719952b4727d84cfc1e46fe69bd9a843c5
kshkumar <kshkumar@adobe.com>
2019-12-25 12:58:17 +0530

    Missing duplicates using elimination at the nit level.

--- epi_judge_java/epi/AbsentValueArray.java
+++ epi_judge_java/epi/AbsentValueArray.java
@@ -1,11 +1,40 @@
 package epi;
+import java.util.BitSet;
+import java.util.Iterator;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class AbsentValueArray {
 
+  static int COUNT = 1 << 16;
   @EpiTest(testDataFile = "absent_value_array.tsv")
   public static int findMissingElement(Iterable<Integer> stream) {
-    // TODO - you fill in here.
+    int[] cnt = new int[COUNT];
+    Iterator<Integer> it = stream.iterator();
+    while(it.hasNext()){
+      int x = it.next();
+      cnt[x >>> 16]++;
+    }
+
+    for(int i=0;i<COUNT;++i){
+      if(cnt[i] < COUNT){
+        it = stream.iterator();
+        BitSet bitSet = new BitSet(COUNT);
+        while(it.hasNext()){
+          int x = it.next();
+          if((x >>> 16) == i){
+            bitSet.set((COUNT - 1) & x);
+          }
+        }
+
+        for(int j=0;j<COUNT;++j){
+          if(!bitSet.get(j)){
+            return (i << 16) | j;
+          }
+        }
+      }
+    }
+
     return 0;
   }
 
--- epi_judge_java/epi/RealSquareRoot.java
+++ epi_judge_java/epi/RealSquareRoot.java
@@ -5,8 +5,40 @@ public class RealSquareRoot {
   @EpiTest(testDataFile = "real_square_root.tsv")
 
   public static double squareRoot(double x) {
-    // TODO - you fill in here.
-    return 0.0;
+    double beg, en;
+    if(x < 1.0){
+      beg = x;
+      en = 1.0;
+      }
+      else{//x>= 1.<9.
+      beg = 1.0;
+      en = x;
+    }
+
+    while(compare(beg, en) != 0){
+      double mid = beg + (en - beg) * 0.5;
+      double midsqr = mid * mid;
+      if(compare(midsqr, x) > 0){
+        en = mid;
+      }
+      else {
+          beg = mid;
+        }
+    }
+    return beg;
+  }
+
+  private static int compare(double a, double b){
+    double EPSILON = 0.000001;
+    double diff = (a - b) / b;
+
+    if(diff < -EPSILON)
+      return -1;
+
+    if(diff > EPSILON)
+      return 1;
+
+    return 0;
   }
 
   public static void main(String[] args) {
